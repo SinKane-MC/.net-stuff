c4920dced128e426ce4376f1514cbb9bb1d52cf6
SinKane <wade.grimm@shaw.ca>
2019-12-09 21:03:11 -0700

    RateCalc updates

---
+++ C#/RateCalc-PowerCo/RateCalc-PowerCo/frmMain.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace RateCalc_PowerCo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radIndustrial = new System.Windows.Forms.RadioButton();
+            this.radCommercial = new System.Windows.Forms.RadioButton();
+            this.radRes = new System.Windows.Forms.RadioButton();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.txtOffPeak = new System.Windows.Forms.TextBox();
+            this.lblOffPeak = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 24);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(179, 202);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(215, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Client Rate Calculator";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radIndustrial);
+            this.groupBox1.Controls.Add(this.radCommercial);
+            this.groupBox1.Controls.Add(this.radRes);
+            this.groupBox1.Location = new System.Drawing.Point(219, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 121);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client Type";
+            // 
+            // radIndustrial
+            // 
+            this.radIndustrial.AutoSize = true;
+            this.radIndustrial.Location = new System.Drawing.Point(7, 79);
+            this.radIndustrial.Name = "radIndustrial";
+            this.radIndustrial.Size = new System.Drawing.Size(102, 28);
+            this.radIndustrial.TabIndex = 2;
+            this.radIndustrial.TabStop = true;
+            this.radIndustrial.Text = "Industrial";
+            this.radIndustrial.UseVisualStyleBackColor = true;
+            this.radIndustrial.CheckedChanged += new System.EventHandler(this.radIndustrial_CheckedChanged);
+            // 
+            // radCommercial
+            // 
+            this.radCommercial.AutoSize = true;
+            this.radCommercial.Location = new System.Drawing.Point(7, 54);
+            this.radCommercial.Name = "radCommercial";
+            this.radCommercial.Size = new System.Drawing.Size(129, 28);
+            this.radCommercial.TabIndex = 1;
+            this.radCommercial.TabStop = true;
+            this.radCommercial.Text = "Commercial";
+            this.radCommercial.UseVisualStyleBackColor = true;
+            // 
+            // radRes
+            // 
+            this.radRes.AutoSize = true;
+            this.radRes.Location = new System.Drawing.Point(7, 29);
+            this.radRes.Name = "radRes";
+            this.radRes.Size = new System.Drawing.Size(120, 28);
+            this.radRes.TabIndex = 0;
+            this.radRes.TabStop = true;
+            this.radRes.Text = "Residential";
+            this.radRes.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(219, 183);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(105, 44);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(219, 236);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(105, 44);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(584, 236);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(105, 44);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "&Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(459, 68);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(94, 24);
+            this.lblInput.TabIndex = 6;
+            this.lblInput.Text = "Input kWh";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(589, 68);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 29);
+            this.txtInput.TabIndex = 7;
+            this.txtInput.Text = "0";
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.ForeColor = System.Drawing.Color.Green;
+            this.lblAmount.Location = new System.Drawing.Point(463, 162);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(226, 44);
+            this.lblAmount.TabIndex = 8;
+            this.lblAmount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtOffPeak
+            // 
+            this.txtOffPeak.Location = new System.Drawing.Point(589, 113);
+            this.txtOffPeak.Name = "txtOffPeak";
+            this.txtOffPeak.Size = new System.Drawing.Size(100, 29);
+            this.txtOffPeak.TabIndex = 10;
+            this.txtOffPeak.Text = "0";
+            this.txtOffPeak.Visible = false;
+            // 
+            // lblOffPeak
+            // 
+            this.lblOffPeak.AutoSize = true;
+            this.lblOffPeak.Location = new System.Drawing.Point(459, 113);
+            this.lblOffPeak.Name = "lblOffPeak";
+            this.lblOffPeak.Size = new System.Drawing.Size(123, 24);
+            this.lblOffPeak.TabIndex = 9;
+            this.lblOffPeak.Text = "Off-peak kWh";
+            this.lblOffPeak.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(730, 318);
+            this.Controls.Add(this.txtOffPeak);
+            this.Controls.Add(this.lblOffPeak);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frmMain";
+            this.Text = "Calculate Rate";
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radIndustrial;
+        private System.Windows.Forms.RadioButton radCommercial;
+        private System.Windows.Forms.RadioButton radRes;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.TextBox txtOffPeak;
+        private System.Windows.Forms.Label lblOffPeak;
+    }
+}
+
---
+++ C#/RateCalc-PowerCo/RateCalc-PowerCo/frmMain.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RateCalc_PowerCo
+{
+    public partial class frmMain : Form
+    {
+        const decimal RES_FLAT = 6.00m, RES_PER_HOUR = 0.52m;
+        const decimal COM_FLAT = 60.00m, COM_PER_HOUR = 0.045m;
+        const int BASE_HOURS = 1000;
+        const decimal IND_PK_FLAT = 76.00m, IND_PK_PER_HOUR = 0.065m, IND_OP_FLAT = 40.00m, IND_OP_PER_HOUR = 0.028m;
+        public frmMain()
+        {
+            InitializeComponent();
+
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            txtInput.Focus();
+        }
+
+        private void radIndustrial_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radIndustrial.Checked)
+            {
+                lblOffPeak.Visible = true;
+                txtOffPeak.Visible = true;
+                lblInput.Text = "Peak kWh";
+            }
+            else
+            {
+                lblOffPeak.Visible = false;
+                txtOffPeak.Visible = false;
+                lblInput.Text = "Input kWh";
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            radRes.Select();
+            txtInput.Text = "0";
+            lblInput.Text = "Input kWh";
+            lblAmount.Text = "";
+            //btnCalculate.Enabled = false;
+            txtOffPeak.Text = "0";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (radRes.Checked)
+            {
+                decimal resAmount;
+                resAmount = RES_FLAT + (RES_PER_HOUR * Convert.ToDecimal(txtInput.Text));
+                lblAmount.Text = resAmount.ToString("c");
+            }else if (radCommercial.Checked)
+            {
+                decimal comAmount;
+                int comHours;
+                if (Convert.ToInt32(txtInput.Text) > BASE_HOURS)
+                {
+                    comHours = Convert.ToInt32(txtInput.Text) - BASE_HOURS;
+                    comAmount = comHours * COM_PER_HOUR;
+                    comAmount += COM_FLAT;
+                }
+                else
+                {
+                    comAmount = COM_FLAT;
+                }
+                lblAmount.Text = comAmount.ToString("c");
+            }else if (radIndustrial.Checked)
+            {
+                decimal indAmountPK, indAmountOP,indTotal;
+                int indHoursPK, indHoursOP;
+                if (Convert.ToInt32(txtInput.Text) > BASE_HOURS)
+                {
+                    indHoursPK = Convert.ToInt32(txtInput.Text) - BASE_HOURS;
+                    indAmountPK = (indHoursPK * IND_PK_PER_HOUR)+IND_PK_FLAT;
+                    
+                }
+                else
+                {
+                    indAmountPK = IND_PK_FLAT;
+                }
+                if (Convert.ToInt32(txtOffPeak.Text) > BASE_HOURS)
+                {
+                    indHoursOP = Convert.ToInt32(txtOffPeak.Text) - BASE_HOURS;
+                    indAmountOP = (indHoursOP * IND_OP_PER_HOUR) + IND_OP_FLAT;
+                }else
+                {
+                    indAmountOP = IND_OP_FLAT;
+                }
+                indTotal = indAmountPK + indAmountOP;
+                lblAmount.Text = indTotal.ToString("c");
+            }
+            
+        }
+
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            //btnCalculate.Enabled = !string.IsNullOrWhiteSpace(txtInput.Text);
+        }
+
+       
+    }
+}
